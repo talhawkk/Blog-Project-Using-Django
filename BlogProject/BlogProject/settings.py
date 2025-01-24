aac8c179e85cd2ee173aed16963cc7422722fbd5
Talhak <talhawkk@gmail.com>
2025-01-25 01:20:18 +0500

    Added Railway URL to ALLOWED_HOSTS

--- BlogProject/BlogProject/settings.py
+++ BlogProject/BlogProject/settings.py
@@ -135,3 +135,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    'https://blog-project-using-django-production.up.railway.app',  # Add your production URL here
+]
